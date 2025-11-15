55e1b2cc367fa195d4e4541e09d7702662a02977
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-11-14 20:20:27 -0500

    Add PokemonCasaniers class implementation
    Introduces the PokemonCasaniers class, inheriting from Pokemon. Adds properties for number of legs, size, and hours of TV watched, and overrides methods for calculating speed and displaying information.

---
+++ classes/PokemonCasaniers.cs
@@ -0,0 +1,29 @@
+﻿namespace OOPTP2.classes
+{
+    class PokemonCasaniers : Pokemon
+    {
+        private int nbrPattes;
+        private float taille;
+        private int heuresDeVisionnage;
+
+        public PokemonCasaniers(string nom, float poids, int nbrPattes, float taille, int heuresDeVisionnage) : base(nom, poids)
+        {
+            this.taille = taille;
+            this.nbrPattes = nbrPattes;
+            this.heuresDeVisionnage = heuresDeVisionnage;
+        }
+
+        public override float ClacVitesse()
+        {
+            //formule : vitesse = nombre de pattes *taille * 3
+            vitesse = nbrPattes * taille * 3;
+            return vitesse;
+
+        }
+
+        public override void Afficher()
+        {
+            Console.WriteLine($"Je suis le pokémon {nom} mon poids est de {poids} kg, ma vitesse est de {ClacVitesse():F2} km/h j'ai {nbrPattes} pattes, ma taille est de {taille}m  je regarde la télé {heuresDeVisionnage}h par jour");
+        }
+    }
+}
