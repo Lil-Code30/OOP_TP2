2d96a4e9b080c60584188525b8daf8ee9a1983f7
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-11-14 19:07:40 -0500

    Add Pokemon base class and update project structure
    Introduced an abstract Pokemon class with core attributes and static management methods in classes/Pokemon.cs. Updated Program.cs to use the new class structure, added UML diagrams, and improved .gitignore for VS-related files.

---
+++ classes/Pokemon.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace OOPTP2.classes
+{
+    abstract class  Pokemon
+    {
+        protected string nom;
+        protected int poids;
+        protected float vitesse;
+        private static List<Pokemon> listPokemons = new List<Pokemon>();
+        
+
+        public Pokemon(string nom, int poids)
+        {
+            this.nom = nom;
+            this.poids = poids;
+        }
+
+        abstract public float ClacVitesse();
+        abstract public void Afficher();
+
+        //une méthode qui affiche tous les Pokemons créés
+        public static void AfficherTousPokemon()
+        {
+            Console.WriteLine("Liste des Pokemon");
+            foreach(Pokemon pokemon in listPokemons)
+            {
+                Console.WriteLine(pokemon);
+            }
+        }
+
+
+        //une méthode qui calcule la vitesse moyenne des Pokemons créés.
+        public static void VitesseMoyen()
+        {
+            float totalVitesse = 0;
+            int countPokemon = listPokemons.Count;
+            foreach (Pokemon pokemon in listPokemons)
+            {
+
+                totalVitesse = totalVitesse + pokemon.vitesse;
+
+            }
+
+            float vitesseMoyen = totalVitesse / countPokemon;
+            Console.WriteLine($"La vitesse moyen des pokemons est :{vitesseMoyen} Km/h");
+        }
+
+        //une méthode qui ajoute un Pokemon
+        public static void AjoutePokemon(Pokemon pokemon) {
+            listPokemons.Add(pokemon);
+        }
+
+
+        //une méthode qui affiche le nombre de Pokemons de chaque catégorie
+        public static void AfficherPokemonParCat()
+        {
+            Console.WriteLine("To be implemented");
+        }
+
+
+    }
+}
