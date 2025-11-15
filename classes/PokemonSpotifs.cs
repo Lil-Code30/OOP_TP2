4c4e1604120614c461d0c4598c0936666a347434
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-11-14 19:56:03 -0500

    Create PokemonSpotifs.cs

---
+++ classes/PokemonSpotifs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOPTP2.classes
+{
+    class PokemonSpotifs : Pokemon
+    {
+        private int nbrPattes;
+        private float taille;
+        private int frequenceCardiaque;
+
+        public PokemonSpotifs(string nom, float poids,int nbrPattes, float taille, int frequenceCardiaque) : base(nom, poids)
+        {
+            this.taille = taille;
+            this.nbrPattes = nbrPattes;
+            this.frequenceCardiaque = frequenceCardiaque;
+        }
+
+        public override float ClacVitesse()
+        {
+            //formule : vitesse = nombre de pattes *taille * 3
+            vitesse = nbrPattes * taille * 3;
+            return vitesse;
+            
+        }
+
+        public override void Afficher()
+        {
+            Console.WriteLine($"Je suis le pokémon {nom} mon poids est de {poids} kg, ma vitesse est de {ClacVitesse():F2} km/h j'ai {nbrPattes} pattes, ma taille est de {taille}m ma fréquence cardiaque est de {frequenceCardiaque} pulsations à la minute");
+        }
+
+    }
+}
