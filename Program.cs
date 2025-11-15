e453b39d85191eae6f2b0527b72a6f81a9387125
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-11-14 21:05:09 -0500

    Add multiple Pokemon instances to main program
    Added two instances each of PokemonSpotifs, PokemonCasaniers, PokemonDeMers, and PokemonCrosiere in Program.cs. Each instance is created and added to the Pokemon collection for further use.

--- Program.cs
+++ Program.cs
@@ -7,17 +7,33 @@ class Program
     {
         Console.WriteLine("...");
 
+        // PokemonSpotifs - 2 instances
         PokemonSpotifs pika = new PokemonSpotifs("Pikachu", 18, 2, 0.85f, 120);
         Pokemon.AjoutePokemon(pika);
 
+        PokemonSpotifs evoli = new PokemonSpotifs("Evoli", 6.5f, 4, 0.30f, 110);
+        Pokemon.AjoutePokemon(evoli);
+
+        // PokemonCasaniers - 2 instances
         PokemonCasaniers sala = new PokemonCasaniers("Salameche", 12, 2, 0.65f, 8);
         Pokemon.AjoutePokemon(sala);
 
+        PokemonCasaniers ronflex = new PokemonCasaniers("Ronflex", 460, 2, 2.10f, 16);
+        Pokemon.AjoutePokemon(ronflex);
+
+        // PokemonDeMers - 2 instances
         PokemonDeMers rondou = new PokemonDeMers("Rondoudou", 45, 2);
         Pokemon.AjoutePokemon(rondou);
 
+        PokemonDeMers carapuce = new PokemonDeMers("Carapuce", 9, 2);
+        Pokemon.AjoutePokemon(carapuce);
+
+        // PokemonCrosiere - 2 instances
         PokemonCrosiere bul = new PokemonCrosiere("Bulbizarre", 15, 3);
         Pokemon.AjoutePokemon(bul);
+
+        PokemonCrosiere leviator = new PokemonCrosiere("Leviator", 235, 4);
+        Pokemon.AjoutePokemon(leviator);
 
 
         pika.Afficher();
