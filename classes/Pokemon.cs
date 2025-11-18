8276f6d84f127b2dde6717df4a5ca5b49a8d0f49
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-11-18 12:49:53 -0500

    Refactorisation : séparation des responsabilités
    Externalisation de la gestion des Pokémons dans une nouvelle classe `Pokemons` :
    - Ajout de la classe `Pokemons` dans `Pokemons.cs` pour gérer la liste des Pokémons et les opérations globales (ajout, affichage, statistiques).
    - Simplification de la classe abstraite `Pokemon` en supprimant la gestion globale des Pokémons et en ajoutant des propriétés pour accéder aux attributs protégés.
    - Mise à jour de la classe `Program` pour utiliser la nouvelle classe `Pokemons` à la place des méthodes statiques de `Pokemon`.
    
    Ces modifications améliorent la modularité, la lisibilité et la séparation des responsabilités dans le code.

--- classes/Pokemon.cs
+++ classes/Pokemon.cs
@@ -5,108 +5,30 @@
         protected string nom;
         protected float poids;
         protected float vitesse;
-        private static List<Pokemon> listPokemons = new List<Pokemon>();
         
-
         public Pokemon(string nom, float poids)
         {
             this.nom = nom;
             this.poids = poids;
-            vitesse = 0;
+            vitesse = CalcVitesse();
+        }
+
+        public string Nom
+        {
+                       get { return nom; }
+        }
+
+        public float Poids
+        {
+            get { return poids; }
+        }
+
+        public float Vitesse
+        {
+            get { return vitesse; }
         }
 
         abstract public float CalcVitesse();
         abstract public void Afficher();
-
-        //une méthode qui affiche tous les Pokemons créés
-        public static void AfficherTousPokemon()
-        {
-            int count = 1;
-           
-            Console.WriteLine("\n╔══════════════════════════════════════════════════════╗");
-            Console.WriteLine("║                 Liste des Pokemons                   ║");
-            Console.WriteLine("╚══════════════════════════════════════════════════════╝");
-            foreach (Pokemon pokemon in listPokemons)
-            {
-                Console.WriteLine($"{count} - Pokemon '{pokemon.nom}' de type {pokemon.GetType()}");
-                
-                count++;
-            }
-            Console.WriteLine("════════════════════════════════════════════════════════");
-            Console.WriteLine();
-        }
-
-        //Afficher le nombre total des Pokemons créés
-        public static void AfficherNbrPokemon()
-        {
-            Console.WriteLine($"Le nombre total des Pokemons créés");
-            Console.WriteLine("╔════════════╦══════════════════════╗");
-            Console.WriteLine($"║   Total #  ║ {listPokemons.Count,2} Pokemon(s) {" ",7}║");
-            Console.WriteLine("╚════════════╩══════════════════════╝");
-            Console.WriteLine();
-        }
-
-
-        //une méthode qui calcule la vitesse moyenne des Pokemons créés.
-        public static void VitesseMoyen()
-        {
-            float totalVitesse = 0;
-            int countPokemon = listPokemons.Count;
-            foreach (Pokemon pokemon in listPokemons)
-            {
-
-                totalVitesse = totalVitesse + pokemon.vitesse;
-
-            }
-
-            float vitesseMoyen = totalVitesse / countPokemon;
-            Console.WriteLine($"La vitesse moyen des pokemons.");
-            Console.WriteLine("╔════════════╦══════════════════════╗");
-            Console.WriteLine($"║   Moyen #  ║ {vitesseMoyen:F2} Km/h {" ",11}║");
-            Console.WriteLine("╚════════════╩══════════════════════╝");
-            Console.WriteLine();
-        }
-
-        //une méthode qui ajoute un Pokemon
-        public static void AjoutePokemon(Pokemon pokemon) {
-            listPokemons.Add(pokemon);
-        }
-
-
-        //une méthode qui affiche le nombre de Pokemons de chaque catégorie
-        public static void AfficherNbrPokemonParCategorie()
-        {
-          
-            Dictionary<string, int> categoriePokemon = new Dictionary<string, int>
-            {
-                {"PokemonSpotifs" , 0},
-                {"PokemonCasaniers" , 0},
-                {"PokemonDeMers" , 0},
-                {"PokemonCrosiere" , 0}
-            };
-
-            foreach (Pokemon pokemon in listPokemons)
-            {
-                
-                var pokemonType = Convert.ToString(pokemon.GetType());
-                string nomCategorie = pokemonType.Split(".").Last();
-
-                if (categoriePokemon.ContainsKey(nomCategorie)){
-                    categoriePokemon[nomCategorie]++;
-                }
-            }
-
-            Console.WriteLine("\n╔══════════════════════════════════════════════════════╗");
-            Console.WriteLine("║         Nombre de Pokemon par catégorie              ║");
-            Console.WriteLine("╠══════════════════════════════════════════════════════╣");
-            foreach (var categorie in categoriePokemon)
-            {
-                Console.WriteLine($"║ {categorie.Key,-30} : {categorie.Value,8} Pokemon(s) ║");
-            }
-            Console.WriteLine("╚══════════════════════════════════════════════════════╝");
-            Console.WriteLine();
-        }
-
-
     }
 }
